fbc4b55ee1e5d30336a107c5f3a3034864d050f4
Ahmad Shabbir <itzahmadshabbirhere@gmail.com>
2025-04-04 16:54:22 -0400

    Initial Commit

--- src/com/concurrency/commontasks/Main.java
+++ src/com/concurrency/commontasks/Main.java
@@ -10,22 +10,5 @@ public class Main {
         List<Integer> flatList = CSVReader.readFlatList(filePath);
         int[][] matrix = CSVReader.readMatrix(filePath, 20, 10);
 
-        double stdDev = StandardDeviationCalculator.calculate(flatList);
-        System.out.println("Standard Deviation: " + stdDev);
-
-        int[][] matrixA = new int[10][10];
-        int[][] matrixB = new int[10][10];
-        System.arraycopy(matrix, 0, matrixA, 0, 10);
-        System.arraycopy(matrix, 10, matrixB, 0, 10);
-
-        int[][] product = MatrixMultiplier.multiply(matrixA, matrixB);
-        System.out.println("Matrix multiplication result:");
-        for (int[] row : product) {
-            for (int val : row) System.out.print(val + " ");
-            System.out.println();
-        }
-
-        List<Integer> sorted = MergeSorter.concurrentMergeSort(flatList);
-        System.out.println("Sorted data (desc): " + sorted);
     }
 }
